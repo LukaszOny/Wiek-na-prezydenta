947768e87f0f858f109f1bd3a84233009bb3ca39
lonys <lonys@ONYSZEK>
2023-11-11 14:32:54 +0100

    Add project files.

---
+++ Wiek na prezydenta/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Proszę, wprowadż swój wiek");
+
+int a = Int32.Parse(Console.ReadLine());
+
+if (a > 20)
+{
+    Console.WriteLine("Gratulacje, możesz ubiegać się o stanowisko Posła RP");
+
+}
+
+
+if (a > 29)
+{
+    Console.WriteLine("Gratulacje, możesz ubiegać się o stanowisko Senatora RP");
+
+}
+
+if (a > 34)
+{
+    Console.WriteLine("Gratulacje, możesz ubiegać się o stanowisko Prezydenta RP");
+}
